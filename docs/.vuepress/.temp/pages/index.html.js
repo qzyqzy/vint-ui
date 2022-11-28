8fd9399fe211f2e75fc3cd0c1604768d2f389724
qzyqzy <qiuzhenyu@hangzhouyq.com>
2022-11-28 14:39:40 +0800

    文档部署

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,14 +1,1 @@
 export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"Vint UI\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[{\"level\":2,\"title\":\"安装\",\"slug\":\"安装\",\"link\":\"#安装\",\"children\":[]},{\"level\":2,\"title\":\"使用\",\"slug\":\"使用\",\"link\":\"#使用\",\"children\":[]},{\"level\":2,\"title\":\"License\",\"slug\":\"license\",\"link\":\"#license\",\"children\":[]}],\"git\":{\"updatedTime\":1669615533000,\"contributors\":[{\"name\":\"qzyqzy\",\"email\":\"qiuzhenyu@hangzhouyq.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
