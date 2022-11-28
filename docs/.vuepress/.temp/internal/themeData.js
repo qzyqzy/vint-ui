fa62078898a64f96b0cc8e3744d48b237945dd63
qzyqzy <qiuzhenyu@hangzhouyq.com>
2022-11-28 14:23:52 +0800

    配置base

--- docs/.vuepress/.temp/internal/themeData.js
+++ docs/.vuepress/.temp/internal/themeData.js
@@ -1,1 +1,14 @@
-export const themeData = JSON.parse("{\"navbar\":[{\"text\":\"Home\",\"link\":\"/\"},{\"text\":\"Github\",\"link\":\"https://github.com/qzyqzy/deft-ui\"},{\"text\":\"Npm\",\"link\":\"https://www.npmjs.com/package/deft-ui\"}],\"sidebar\":[\"/\",{\"text\":\"CSS 组件\",\"collapsible\":false,\"children\":[\"/md/cssComponents/button\"]}],\"locales\":{\"/\":{\"selectLanguageName\":\"English\"}},\"colorMode\":\"auto\",\"colorModeSwitch\":true,\"logo\":null,\"repo\":null,\"selectLanguageText\":\"Languages\",\"selectLanguageAriaLabel\":\"Select language\",\"sidebarDepth\":2,\"editLink\":true,\"editLinkText\":\"Edit this page\",\"lastUpdated\":true,\"lastUpdatedText\":\"Last Updated\",\"contributors\":true,\"contributorsText\":\"Contributors\",\"notFound\":[\"There's nothing here.\",\"How did we get here?\",\"That's a Four-Oh-Four.\",\"Looks like we've got some broken links.\"],\"backToHome\":\"Take me home\",\"openInNewWindow\":\"open in new window\",\"toggleColorMode\":\"toggle color mode\",\"toggleSidebar\":\"toggle sidebar\"}")
+export const themeData = JSON.parse("{\"navbar\":[{\"text\":\"Home\",\"link\":\"/\"},{\"text\":\"Github\",\"link\":\"https://github.com/qzyqzy/vint-ui\"},{\"text\":\"Npm\",\"link\":\"https://www.npmjs.com/package/vint-ui\"}],\"sidebar\":[\"/\",{\"text\":\"CSS 组件\",\"collapsible\":false,\"children\":[\"/md/cssComponents/button\"]}],\"locales\":{\"/\":{\"selectLanguageName\":\"English\"}},\"colorMode\":\"auto\",\"colorModeSwitch\":true,\"logo\":null,\"repo\":null,\"selectLanguageText\":\"Languages\",\"selectLanguageAriaLabel\":\"Select language\",\"sidebarDepth\":2,\"editLink\":true,\"editLinkText\":\"Edit this page\",\"lastUpdated\":true,\"lastUpdatedText\":\"Last Updated\",\"contributors\":true,\"contributorsText\":\"Contributors\",\"notFound\":[\"There's nothing here.\",\"How did we get here?\",\"That's a Four-Oh-Four.\",\"Looks like we've got some broken links.\"],\"backToHome\":\"Take me home\",\"openInNewWindow\":\"open in new window\",\"toggleColorMode\":\"toggle color mode\",\"toggleSidebar\":\"toggle sidebar\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateThemeData) {
+    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ themeData }) => {
+    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
+  })
+}
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,13 +2,14 @@ import { defaultTheme } from "vuepress";
 import { demoblockPlugin } from "vuepress-plugin-demoblock-plus";
 export default {
   lang: "zh-CN",
+  base: "/vint-ui/",
   title: "您好！",
   plugins: [demoblockPlugin()],
   theme: defaultTheme({
     navbar: [
       { text: "Home", link: "/" },
-      { text: "Github", link: "https://github.com/qzyqzy/deft-ui" },
-      { text: "Npm", link: "https://www.npmjs.com/package/deft-ui" },
+      { text: "Github", link: "https://github.com/qzyqzy/vint-ui" },
+      { text: "Npm", link: "https://www.npmjs.com/package/vint-ui" },
     ],
     sidebar: [
       "/",
